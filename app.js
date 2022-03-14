541f63676e0fbf1112b4956dac092b771106dcc8
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-03-13 19:45:03 -0700

    create and position ball

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 560;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -66,17 +67,26 @@ function drawUser() {
 function moveUser(e) {
   switch (e.key) {
     case "ArrowLeft":
-      currentPosition[0] -= 10;
-      drawUser();
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
+      }
       break;
     case "ArrowRight":
-      currentPosition[0] += 10;
-      drawUser();
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
       break;
   }
 }
 
 document.addEventListener("keydown", moveUser);
+
+//add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+grid.appendChild(ball);
 
 /*  over all construction of mechanics
     1. grab grid
