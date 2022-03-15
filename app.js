1b3d947404fd07818351bb97ceadbe2523bb4cad
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-03-15 13:51:25 -0700

    yAxis issue resolved

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
 const boardWidth = 560;
+const boardHeight = 300;
+let xDirection = -2;
+let yDirection = 2;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -9,13 +14,15 @@ let currentPosition = userStart;
 const ballStart = [270, 40];
 let ballCurrentPosition = ballStart;
 
+let timerId;
+
 //create individual block
 class Block {
   constructor(xAxis, yAxis) {
     this.bottomLeft = [xAxis, yAxis];
     this.bottomRight = [xAxis + blockWidth, yAxis];
-    this.topLeft = [xAxis, yAxis + blockHeight];
     this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    this.topLeft = [xAxis, yAxis + blockHeight];
   }
 }
 
@@ -48,6 +55,7 @@ function addBlocks() {
     block.style.left = blocks[i].bottomLeft[0] + "px";
     block.style.bottom = blocks[i].bottomLeft[1] + "px";
     grid.appendChild(block);
+    console.log(blocks[i].bottomLeft);
   }
 }
 
@@ -55,21 +63,15 @@ addBlocks();
 
 //add user
 const user = document.createElement("div");
-drawUser();
 user.classList.add("user");
-
 grid.appendChild(user);
+drawUser();
 
-//draw the user
-function drawUser() {
-  user.style.left = currentPosition[0] + "px";
-  user.style.bottom = currentPosition[1] + "px";
-}
-
-function drawBall() {
-  ball.style.left = ballCurrentPosition[0] + "px";
-  ball.style.bottom = ballCurrentPosition[1] + "px";
-}
+//add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+grid.appendChild(ball);
+drawBall();
 
 //move user
 function moveUser(e) {
@@ -77,12 +79,14 @@ function moveUser(e) {
     case "ArrowLeft":
       if (currentPosition[0] > 0) {
         currentPosition[0] -= 10;
+        console.log(currentPosition[0] > 0);
         drawUser();
       }
       break;
     case "ArrowRight":
       if (currentPosition[0] < boardWidth - blockWidth) {
         currentPosition[0] += 10;
+        console.log(currentPosition[0]);
         drawUser();
       }
       break;
@@ -90,20 +94,66 @@ function moveUser(e) {
 }
 document.addEventListener("keydown", moveUser);
 
-//add ball
-const ball = document.createElement("div");
-ball.classList.add("ball");
-drawBall();
-grid.appendChild(ball);
+//draw the user
+function drawUser() {
+  user.style.left = currentPosition[0] + "px";
+  user.style.bottom = currentPosition[1] + "px";
+}
+
+//draw the ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + "px";
+  ball.style.bottom = ballCurrentPosition[1] + "px";
+}
 
 //move the ball
 function moveBall() {
-  ballCurrentPosition[0] += 2;
-  ballCurrentPosition[1] += 2;
+  ballCurrentPosition[0] += xDirection;
+  ballCurrentPosition[1] += yDirection;
   drawBall();
+  checkForCollisions();
 }
 
-setInterval(moveBall, 30);
+timerId = setInterval(moveBall, 30);
+
+// check for collisions
+function checkForCollisions() {
+  // check for wall collisions
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter ||
+    ballCurrentPosition[0] <= 0
+  ) {
+    changeDirection();
+  }
+
+  //check for game over
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = "Game Over";
+    document.removeEventListener("keydown", moveUser);
+  }
+
+  function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) {
+      yDirection = -2;
+      return;
+    }
+    if (xDirection === 2 && yDirection === -2) {
+      xDirection = -2;
+      return;
+    }
+    if (xDirection === -2 && yDirection === -2) {
+      yDirection = 2;
+      return;
+    }
+    if (xDirection === -2 && yDirection === 2) {
+      xDirection = 2;
+      return;
+    }
+  }
+}
+
 /*  over all construction of mechanics
     1. grab grid
     2. make function to store block creation in
